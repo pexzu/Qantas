cdefe7414270eda792cd7eedc27bbae07e55d567
VJ <vichuvj92@gmail.com>
2025-02-03 21:43:06 +1100

    Initial React project setup
    Typescript, Styled components and other basics

---
+++ qantas-hotels/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Hotels } from "./Hotels/Hotels";
+
+function App() {
+  return (
+    <>
+      <Hotels />
+    </>
+  );
+}
+
+export default App;
---
+++ qantas-hotels/src/Hotels/Hotels.tsx
@@ -0,0 +1,10 @@
+import React, { use, useEffect, useState } from "react";
+
+export const Hotels = () => {
+  return (
+    <div>
+      tesing
+      <span>something</span>
+    </div>
+  );
+};
